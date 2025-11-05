43d6ea83ea1bdcc14575a08e1a58ea18e6550cb1
Wesley Geraldo de Oliveira da Nóbrega <wesley@ufpi.edu.br>
2025-11-05 16:23:30 -0300

    feat: Configura deploy para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/site-escoladeconselhos/',
   resolve: {
     alias: {
       // Fix: Cannot find name '__dirname'
