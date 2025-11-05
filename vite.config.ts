ffc6f0d475f7ed610cfe5639620d276fd28bf743
Wesley Geraldo de Oliveira da Nóbrega <wesley@ufpi.edu.br>
2025-11-05 17:07:27 -0300

    fix: Limpeza de dependencias do github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Fix: Cannot find name '__dirname'
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
 })
