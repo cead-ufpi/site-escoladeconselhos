72500cf86504ab9c0e298dd5002a0a2885c63195
Wesley Geraldo de Oliveira da Nóbrega <wesley@ufpi.edu.br>
2025-11-05 16:59:32 -0300

    feat: Prepara projeto para deploy na Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/site-escoladeconselhos/',
   resolve: {
     alias: {
       // Fix: Cannot find name '__dirname'
